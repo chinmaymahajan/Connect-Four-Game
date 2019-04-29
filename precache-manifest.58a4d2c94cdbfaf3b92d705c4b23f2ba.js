e6ca6387282e506dfca783e9aaded77d7cc75e2d
chimahaj <chimahaj@cisco.com>
2019-04-29 14:44:27 -0400

    Updates

---
+++ precache-manifest.58a4d2c94cdbfaf3b92d705c4b23f2ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb71dbf628703c1c1720621a58c56a7b",
+    "url": "/Connect-Four-Game/index.html"
+  },
+  {
+    "revision": "ba491765758f76145592",
+    "url": "/Connect-Four-Game/static/css/main.a419590b.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/Connect-Four-Game/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "ba491765758f76145592",
+    "url": "/Connect-Four-Game/static/js/main.e586f1c6.chunk.js"
+  },
+  {
+    "revision": "20a97a207e3b1b8a9d3b",
+    "url": "/Connect-Four-Game/static/js/runtime~main.916913e1.js"
+  }
+]);
